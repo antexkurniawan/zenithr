97afcb786c6d351344b7b5ccb3e0fb78381f0025
Antex Ibrahim <antexkurniawan@gmail.com>
2025-12-22 05:14:00 +0000

    Try fixing this error: `Unhandled Runtime Error: TypeError: Cannot read

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -81,6 +81,7 @@ const SelectContent = React.forwardRef<
         className
       )}
       position={position}
+      onMouseDown={(e) => e.preventDefault()}
       {...props}
     >
       <SelectScrollUpButton />
